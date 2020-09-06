049668a8669aa94924221b850627861e76f88e2e
Are Frode Kvanum <are.frode.kvanum@gmail.com>
2020-09-06 20:19:18 +0200

    general tridiag in seperate func

--- assignment1/scripts/thomas.cpp
+++ assignment1/scripts/thomas.cpp
@@ -5,67 +5,82 @@ using namespace std;
 
 double *initVector(int);
 double **initMatrix(int);
-void destroyMatrix(double, int);
+void generalSolver(double *, double *, double *, double *, double *, int);
 
 int main(int argc, char *argv[]) {
-    int exp;
+    int exponent;
     if (argc <= 1) {
         cout << "Missing command line argument!" << endl;
         cout << "Usage: " << argv[0] << " n\nn is the power in 10^n" << endl;
         exit(1);
     } else {
-        exp = atoi(argv[1]);
+        exponent = atoi(argv[1]);
     }
 
-    int n = pow(10, exp);
-    double *upper = initVector(n);
-    double *lower = initVector(n);
-    double *diag = initVector(n);
-    double *solution = initVector(n+1);
-    double *x = initVector(n+1);
+    int n = pow(10, exponent);
+    double h = 1.0/(n + 1);
+    double *c = initVector(n);
+    double *a = initVector(n);
+    double *b = initVector(n);
+    double *v = initVector(n+1);
+    double *solution = initVector(n);
 
-    x[0] = 0;
-    x[n] = 1;
-    solution[0] = solution[n] = 0;
-
+    v[0] = v[n] = 0;
+    double x = h;
     for (int i = 0; i < n; i++) {
-        upper[i] = -1;
-        lower[i] = -1;
-        diag[i] = 2;
+        c[i] = -1;
+        a[i] = -1;
+        b[i] = 2;
+        solution[i] = h*h*100.0*exp(-10*x);
+        x += h;
     }
 
-    double w;
-    for (int i = 1; i < n; i++) {
-        w = lower[i] / diag[i-1];
-        diag[i] = diag[i] - w*upper[i-1];
-        solution[i] = solution[i] - w*diag[i-1];
-    }
-
-    for (int i = n-1; i >= 1; i--)
-        x[i] = (solution[i] - upper[i]*x[i+1])/diag[i];
+    generalSolver(a, b, c, v, solution, n);
 
     cout << "[ ";
     for (int i = 0; i < n+1; i++)
-        cout << x[i] << ", ";
+        cout << v[i] << ", ";
 
     cout << "]" << endl;
 
     cout << "[ ";
-    double tmp;
-    double anal;
+    double tmp = h;
     for (int i = 0; i < 10; i++) {
-        tmp = 0.1*i;
-        cout << 100*pow(2.7, -10*tmp) << ", "; // dette er skummelt...
+        cout << 1 - tmp + tmp*exp(-10) - exp(-10*tmp) << ", "; // dette er skummelt...
+        tmp += h;
     }
+
     cout << "]" << endl;
 
-    delete [] upper;
-    delete [] lower;
-    delete [] diag;
+    delete [] c;
+    delete [] a;
+    delete [] b;
     delete [] solution;
-    delete [] x;
+    delete [] v;
 
     return 0;
+}
+
+void generalSolver(double *a, double *b, double *c, double *v, double *sol, int n) {
+    double *diag, *f;
+    diag = new double[n];
+    f = new double[n];
+    double weight;
+    diag[0] = b[0];
+    f[0] = sol[0];
+
+    for (int i = 1; i < n; i++) {
+        weight = a[i-1] / diag[i-1];
+        diag[i] = b[i] - weight*c[i-1];
+        f[i] = sol[i] - weight*f[i-1];
+    }
+
+    v[n-1] = f[n-1]/diag[n-1];
+    for (int i = n-2; i >= 1; i--)
+        v[i] = (f[i] - c[i]*v[i+1])/diag[i];
+
+    delete [] diag;
+    delete [] f;
 }
 
 double *initVector(int N) {
@@ -79,10 +94,4 @@ double **initMatrix(int N) {
         A[i] = new double[N];
     }
     return A;
-}
-
-void destroyMatrix(double **A, int N) {
-    for (int i = 0; i < N; i++)
-        delete [] A[i];
-    delete [] A;
 }
